26656599bcae01bf2b9faa67462140b24d37e29a
suresh <suresh.ameda@conduent.com>
2020-08-14 20:10:42 +0530

    Eurekaserver code

---
+++ EurekaServer/src/test/java/com/conduent/core2/batch/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conduent.core2.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
